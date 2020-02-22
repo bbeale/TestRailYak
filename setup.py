397fa62ad9295677ce05435ee9cfc21f66442539
bbeale <ben.beale@medhub.com>
2020-02-22 17:20:45 -0500

    Removed old classes that kept getting added to builds.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
       name="testrail_yak",
-      version="1.0.6",
+      version="1.0.7",
       packages=["testrail_yak"],
       classifiers=[
             "Programming Language :: Python :: 3",
