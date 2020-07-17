c4928456559aee8fcb12d46c125386d8f237fbf5
Ben <beale.ben@gmail.com>
2020-07-17 08:17:28 -0400

    Added configuration unit tests.

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -10,7 +10,57 @@ conf = Configuration(client)
 
 
 def test_get_configs(reqmock):
-    reqmock.get()
+    project_id = 100
+    reqmock.get(f"http://example.testrail.com/index.php?/api/v2/get_configs/{project_id}", text='''[
+        {
+            "configs": [
+                {
+                    "group_id": 1,
+                    "id": 1,
+                    "name": "Chrome"
+                },
+                {
+                    "group_id": 1,
+                    "id": 2,
+                    "name": "Firefox"
+                },
+                {
+                    "group_id": 1,
+                    "id": 3,
+                    "name": "Internet Explorer"
+                }
+            ],
+            "id": 1,
+            "name": "Browsers",
+            "project_id": 1
+        },
+        {
+            "configs": [
+                {
+                    "group_id": 2,
+                    "id": 6,
+                    "name": "Ubuntu 12"
+                },
+                {
+                    "group_id": 2,
+                    "id": 4,
+                    "name": "Windows 7"
+                },
+                {
+                    "group_id": 2,
+                    "id": 5,
+                    "name": "Windows 8"
+                }
+            ],
+            "id": 2,
+            "name": "Operating Systems",
+            "project_id": 1
+        }
+    ]''')
+
+    res = conf.get_configs(project_id=project_id)
+    assert res is not None
+    assert type(res) == list
 
 
 def test_add_config_group(reqmock):
