be5319243f7c8155b0bb6aadbdc7caaaa9161bc1
Ben <beale.ben@gmail.com>
2020-07-19 15:50:49 -0400

    Added tests for milestones.

--- testrail_yak/milestone.py
+++ testrail_yak/milestone.py
@@ -16,7 +16,8 @@ class Milestone(object):
         try:
             result = self.client.send_get(f"get_milestone/{milestone_id}")
         except APIError as error:
-            raise error
+            print(error)
+            raise MilestoneException
         else:
             return result
 
@@ -25,7 +26,8 @@ class Milestone(object):
         try:
             result = self.client.send_get(f"get_milestones/{project_id}")
         except APIError as error:
-            raise error
+            print(error)
+            raise MilestoneException
         else:
             return result
 
@@ -39,7 +41,8 @@ class Milestone(object):
             try:
                 result = self.client.send_post(f"add_milestone/{project_id}", data=data)
             except APIError as error:
-                raise error
+                print(error)
+                raise MilestoneException
             else:
                 return result
 
@@ -53,15 +56,21 @@ class Milestone(object):
             try:
                 result = self.client.send_post(f"update_milestone/{milestone_id}", data=data)
             except APIError as error:
-                raise error
+                print(error)
+                raise MilestoneException
             else:
                 return result
 
     def delete_milestone(self, milestone_id: int):
         """ Deletes an existing milestone. """
         try:
-            result = self.client.send_post(f"update_milestone/{milestone_id}")
+            result = self.client.send_post(f"delete_milestone/{milestone_id}", data=None)
         except APIError as error:
-            raise error
+            print(error)
+            raise MilestoneException
         else:
             return result
+
+
+class MilestoneException(Exception):
+    pass
--- tests/test_milestone.py
+++ tests/test_milestone.py
@@ -10,20 +10,97 @@ m = Milestone(client)
 
 
 def test_get_milestone(reqmock):
-    reqmock.get()
+    milestone_id = 1
+    reqmock.get(f"http://example.testrail.com/index.php?/api/v2/get_milestone/{milestone_id}",
+        status_code=200,
+        text='''{
+            "completed_on": 1389968184,
+            "description": "...",
+            "due_on": 1391968184,
+            "id": 1,
+            "is_completed": true,
+            "name": "Release 1.5",
+            "project_id": 1,
+            "url": "http:///testrail/index.php?/milestones/view/1"
+        }''')
+
+    res = m.get_milestone(milestone_id=milestone_id)
+    assert res is not None
+    assert type(res) == dict
+    assert "completed_on" in res.keys()
+    assert "description" in res.keys()
+    assert "due_on" in res.keys()
+    assert "id" in res.keys()
+    assert "is_completed" in res.keys()
+    assert "name" in res.keys()
+    assert "project_id" in res.keys()
+    assert "url" in res.keys()
 
 
 def test_get_milestones(reqmock):
-    reqmock.get()
+    project_id = 1
+    reqmock.get(f"http://example.testrail.com/index.php?/api/v2/get_milestones/{project_id}",
+        status_code=200,
+        text='''[{
+            "completed_on": 1389968184,
+            "description": "...",
+            "due_on": 1391968184,
+            "id": 1,
+            "is_completed": true,
+            "name": "Release 1.5",
+            "project_id": 1,
+            "url": "http:///testrail/index.php?/milestones/view/1"
+        }]''')
+    res = m.get_milestones(project_id=project_id)
+    assert res is not None
+    assert type(res) == list
+    assert type(res[0]) == dict
 
 
 def test_add_milestone(reqmock):
-    reqmock.post()
+    project_id = 1
+    reqmock.post(f"http://example.testrail.com/index.php?/api/v2/add_milestone/{project_id}",
+        status_code=200,
+        text='''{
+            "completed_on": 1389968184,
+            "description": "...",
+            "due_on": 1391968184,
+            "id": 1,
+            "is_completed": true,
+            "name": "Release 1.5",
+            "project_id": 1,
+            "url": "http:///testrail/index.php?/milestones/view/1"
+        }''')
+
+    data = {"name": "Release 1.5", "description": "...", "due_on": 1391968184}
+    res = m.add_milestone(project_id=project_id, data=data)
+    assert res is not None
 
 
 def test_update_milestone(reqmock):
-    reqmock.post()
+    milestone_id = 1
+    reqmock.post(f"http://example.testrail.com/index.php?/api/v2/update_milestone/{milestone_id}",
+        status_code=200,
+        text='''{
+                "completed_on": 1389968184,
+                "description": "...",
+                "due_on": 1391968184,
+                "id": 1,
+                "is_completed": true,
+                "name": "Release 1.5",
+                "project_id": 1,
+                "url": "http:///testrail/index.php?/milestones/view/1"
+            }''')
+
+    data = {"name": "Release 1.5", "description": "...", "due_on": 1391968184}
+    res = m.update_milestone(milestone_id=milestone_id, data=data)
+    assert res is not None
 
 
 def test_delete_milestone(reqmock):
-    reqmock.post()
+    milestone_id = 1
+    reqmock.post(f"http://example.testrail.com/index.php?/api/v2/delete_milestone/{milestone_id}",
+        status_code=200,
+        text='')
+    res = m.delete_milestone(milestone_id=milestone_id)
+    assert res is not None
